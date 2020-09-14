821e1c09fac0e2bdcef62dc6aa67200492d063c1
Anh Luu <kal@elca.vn>
2020-09-14 17:42:04 +0700

    Auth context

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import SignupScreen from './src/screens/SignupScreen'
 import TrackCreateScreen from './src/screens/TrackCreateScreen'
 import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
+import { Context as AuthContext } from './src/context/AuthContext'
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -25,4 +26,12 @@ const switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthContext>
+      <App />
+    </AuthContext>
+  );
+}
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <View style={styles.containerStyle}>
             <Text h3>Sign Up for Tracker</Text>
             <Spacer />
-            <Input label='Email' />
-            <Input label='Password' />
+            <Input
+
+                label='Email'
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize={false}
+            />
+            <Input
+                label='Password'
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize={false}
+                secureTextEntry
+            />
             <Spacer>
                 <Button title='Sign Up' />
             </Spacer>
