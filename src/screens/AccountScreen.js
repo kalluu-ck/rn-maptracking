9ce674e538e49256273535ce61da498127974aa8
Anh Luu <kal@elca.vn>
2020-09-15 17:39:07 +0700

    Safe Area View

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text, Button } from 'react-native-elements'
+import { SafeAreaView } from 'react-navigation'
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext'
 
@@ -8,12 +9,12 @@ const AccountScreen = ({ navigation }) => {
     const { signout } = useContext(AuthContext);
 
     return (
-        <>
+        <SafeAreaView>
             <Text>AccountScreen Screen!</Text>
             <Spacer>
                 <Button title='Sign Out' onPress={signout} />
             </Spacer>
-        </>
+        </SafeAreaView>
     );
 }
 
