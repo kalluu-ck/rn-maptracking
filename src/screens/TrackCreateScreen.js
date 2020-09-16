aaeea5b55244d03507a33e0e41ad5c7518d00869
Anh Luu <kal@elca.vn>
2020-09-16 11:31:15 +0700

    Track form

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -7,6 +7,7 @@ import { SafeAreaView, withNavigationFocus } from 'react-navigation'
 import Map from '../components/Map'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
+import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({ isFocused }) => {
     const { addLocation } = useContext(LocationContext);
@@ -16,6 +17,7 @@ const TrackCreateScreen = ({ isFocused }) => {
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h3>Create a Track</Text>
             <Map />
+            <TrackForm />
             {error ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
     );
