2f99c62bcba789b0ab1ecb565d3ae94eaa7667a2
Anh Luu <kal@elca.vn>
2020-09-17 15:27:37 +0700

    Cleanup code

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,13 @@
 import '../_mockLocation'
 
 import React, { useContext, useCallback } from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, ScrollView } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView, withNavigationFocus } from 'react-navigation'
 import Map from '../components/Map'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
 import TrackForm from '../components/TrackForm'
-import { ScrollView } from 'react-native-gesture-handler'
 import { FontAwesome } from '@expo/vector-icons';
 
 const TrackCreateScreen = ({ isFocused }) => {
